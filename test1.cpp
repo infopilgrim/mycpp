0e4304966c6d2867bce687022b0db3d29d69661c
wup <wu_p@yinyuefengchao.com>
2019-03-19 11:58:49 +0800

    c

---
+++ test1.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<iostream>
+#include<string.h>
+#define maxn 1010
+
+using namespace std;
+
+int a[maxn];
+int main() 
+{
+	int n, k, first = 1, ct = 1;
+	memset(a, 0, sizeof(a));
+	scanf("%d%d", &n, &k);
+	for (int i=1; i<=k; i++)
+		for (int j=1; j<=n; j++)
+			if (j%i == 0) a[j] = !a[j];
+	for (int i=1; i<=n; i++)
+		if (a[i]) { if (first) first = 0; else printf(" "); printf("%d", i);}
+	printf("\n");
+	printf("%d\n", ct++);
+	return 0;
+}
